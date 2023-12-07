96acd65ccfb4884f6a77288cac92716db69e69b3
rasyidfirmans <rasyidfirmansyah56@student.uns.ac.id>
2023-12-07 16:14:23 +0700

    Menambahkan fungsi penjumlahan

--- index.c
+++ index.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+void penjumlahan(int a, int b, int *sum);
+
 int main()
 {
     int a = 10;
-    printf("%d", a);
+    int b = 5;
+    int *sum;
+
+    penjumlahan(a, b, sum);
+    printf("%d", *sum);
+}
+
+void penjumlahan(int a, int b, int *sum)
+{
+    *sum = (a + b);
 }
